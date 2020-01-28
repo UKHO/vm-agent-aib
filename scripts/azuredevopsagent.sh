b06717baa594c3324c32fbe47af847b93e19d467
Marc Denman <MarcDenman@users.noreply.github.com>
2020-01-28 14:43:53 +0000

    Fix: Correct variable usage

--- scripts/azuredevopsagent.sh
+++ scripts/azuredevopsagent.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 agtLocation=/usr/bin/agt
-mkdir -p agtLocation
+mkdir -p $agtLocation
 
 echo "download agent"
-curl https://vstsagentpackage.azureedge.net/agent/2.150.3/vsts-agent-linux-x64-2.150.3.tar.gz | tar zx -C agtLocation
+curl https://vstsagentpackage.azureedge.net/agent/2.150.3/vsts-agent-linux-x64-2.150.3.tar.gz | tar zx -C $agtLocation
 
 echo "set permissions on agent directory"
-chmod 755 -R agtLocation
+chmod 755 -R $agtLocation

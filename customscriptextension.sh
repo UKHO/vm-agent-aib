65797bb7affdff4ecb8ab9254a406af505539064
Marc Denman <MarcDenman@users.noreply.github.com>
2020-02-02 17:05:43 +0000

    Update customscriptextension.sh

--- customscriptextension.sh
+++ customscriptextension.sh
@@ -1,11 +1,13 @@
+#!/bin/bash
 
 echo "########### CONFIGURING AGENT ###########"
 echo "Allow agent to run as root"
 export AGENT_ALLOW_RUNASROOT="YES"
 
 echo "Setup agent"
+agentName = hostname
 cd /usr/lib/agt
-./config.sh --unattended --url https://dev.azure.com/$1 --auth PAT --token $2 --pool "UKHO Ubuntu 1804" --agent hostname --acceptTeeEula --work _work
+./config.sh --unattended --url https://dev.azure.com/$1 --auth PAT --token $2 --pool "UKHO Ubuntu 1804" --agent $agentName --acceptTeeEula --work _work
 
 echo "Install service for agent"
 ./svc.sh install
@@ -13,11 +15,11 @@ echo "Start service for agent"
 ./svc.sh start
 
 
-echo "########### CONFIGURING DRAINER ###########"
-echo "Reload systemd"
-systemctl daemon-reload
+#echo "########### CONFIGURING DRAINER ###########"
+#echo "Reload systemd"
+#systemctl daemon-reload
 
-echo "Enable drainer for restart"
-systemctl enable azurevmagentdrainer.service
-echo "Start drainer for restart"
-systemctl start azurevmagentdrainer.service
+#echo "Enable drainer for restart"
+#systemctl enable azurevmagentdrainer.service
+#echo "Start drainer for restart"
+#systemctl start azurevmagentdrainer.service

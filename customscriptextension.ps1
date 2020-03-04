3c54500082d97c8067078cbd8a167e300dbbf466
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2020-03-04 12:53:46 +0000

    will install dc, just not database

--- customscriptextension.ps1
+++ customscriptextension.ps1
@@ -17,6 +17,45 @@ $size = $PartionSize + "GB"
 
 Resize-Partition -DiskNumber 0 -PartitionNumer 2 -Size ($size)
 
+# Setup NVD
+New-Item C:\tools -ItemType Directory
+
+Set-Location C:\tools
+$dcversion = "dependency-check-5.1.0-release"
+$dczip = "$dcversion.zip"
+
+## Get zip
+wget https://dl.bintray.com/jeremy-long/owasp/$dczip -OutFile $dczip
+
+Expand-Archive -Path ./$dczip
+
+$dcpath = "C:\tools\$dcversion\dependency-check\bin"
+
+$path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
+[System.Environment]::SetEnvironmentVariable("Path", $path + ";$dcpath", "Machine")
+
+Set-Location $dcpath
+
+## update
+## .\dependency-check.bat --updateonly
+
+## set acl
+$user = "NT AUTHORITY\NETWORK SERVICE"
+##$cachepath = "C:\tools\$dcversion\dependency-check\data\cache"
+##$acl = Get-Acl $cachepath
+
+##$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($user,"FullControl","Allow")
+
+##$acl.SetAccessRule($AccessRule)
+
+##$acl | Set-Acl $cachepath
+
+##Get-ChildItem $cachepath -recurse -Force |% {
+##    $ACLFile = Get-Acl $_.fullname
+##    $ACLFile.SetAccessRule($AccessRule)
+##    $ACLfile | Set-Acl -Path $_.fullname
+##}
+
 # Setup vsts agent
 $data = Invoke-RestMethod -Uri https://api.github.com/repos/microsoft/azure-pipelines-agent/releases/latest | Select Name
 

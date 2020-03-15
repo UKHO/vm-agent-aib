1f4fd6da79a763807bc4718e9a374396c5720ff6
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2020-03-15 22:54:00 +0000

    Setup for file share owasp dc

--- customscriptextension.ps1
+++ customscriptextension.ps1
@@ -5,8 +5,12 @@ $account,
 $PAT,
 [Parameter(Mandatory)]
 $PoolName,
-$AdminAccount,
-$AdminPassword,
+[Parameter(Mandatory)]
+$owaspStorageAccount,
+[Parameter(Mandatory)]
+$owaspUser,
+[Parameter(Mandatory)]
+$owaspPassword,
 $PartitionSize = 128
 )
 
@@ -14,44 +18,22 @@ $size = $PartionSize + "GB"
 
 Resize-Partition -DiskNumber 0 -PartitionNumer 2 -Size ($size)
 
+
 # Setup NVD
-New-Item C:\tools -ItemType Directory
+$connectTestResult = Test-NetConnection -ComputerName "$owaspStorageAccount.file.core.windows.net" -Port 445
+if ($connectTestResult.TcpTestSucceeded) {
+    # Save the password so the drive will persist on reboot
+    cmd.exe /C "cmdkey /add:`"$owaspStorageAccount.file.core.windows.net`" /user:`"$owaspUser`" /pass:`"$owaspPassword`""
+    # Mount the drive
+    New-PSDrive -Name Z -PSProvider FileSystem -Root "\\$owaspStorageAccount.file.core.windows.net\owaspdependencycheck"-Persist
+} else {
+    Write-Error -Message "Unable to reach the Azure storage account via port 445. Check to make sure your organization or ISP is not blocking port 445, or use Azure P2S VPN, Azure S2S VPN, or Express Route to tunnel SMB traffic over a different port."
+}
 
-Set-Location C:\tools
-$dcversion = "dependency-check-5.1.0-release"
-$dczip = "$dcversion.zip"
-
-## Get zip
-wget https://dl.bintray.com/jeremy-long/owasp/$dczip -OutFile $dczip
-
-Expand-Archive -Path ./$dczip
-
-$dcpath = "C:\tools\$dcversion\dependency-check\bin"
+$dcpath = "Z:\dependency-check\bin"
 
 $path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
 [System.Environment]::SetEnvironmentVariable("Path", $path + ";$dcpath", "Machine")
-
-Set-Location $dcpath
-
-## update
-.\dependency-check.bat --updateonly
-
-## set acl
-$user = "NT AUTHORITY\NETWORK SERVICE"
-$cachepath = "C:\tools\$dcversion\dependency-check\data\cache"
-$acl = Get-Acl $cachepath
-
-$AccessRule = New-Object System.Security.AccessControl.FileSystemAccessRule($user,"FullControl","Allow")
-
-$acl.SetAccessRule($AccessRule)
-
-$acl | Set-Acl $cachepath
-
-Get-ChildItem $cachepath -recurse -Force |% {
-    $ACLFile = Get-Acl $_.fullname
-    $ACLFile.SetAccessRule($AccessRule)
-    $ACLfile | Set-Acl -Path $_.fullname
-}
 
 # Setup vsts agent
 $data = Invoke-RestMethod -Uri https://api.github.com/repos/microsoft/azure-pipelines-agent/releases/latest | Select Name

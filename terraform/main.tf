747d1c253b07aba6324926318b5da1d50bd1eca9
abrandon <abrandon@ukho.gov.uk>
2020-07-07 15:35:01 +0100

    terraform backets

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,7 +23,7 @@ resource "azurerm_subnet" "aib-subnet" {
   resource_group_name                            = azurerm_resource_group.aib-rg.name
   virtual_network_name                           = azurerm_virtual_network.aib-vnet.name
   enforce_private_link_endpoint_network_policies = true
-  address_prefix                               = ["10.1.2.0/25"]
+  address_prefix                                 = "10.1.2.0/25"
 }
 
 resource "azurerm_network_security_group" "aib-nsg" {

c05a211b8e5872005df48ec0eb321c07c36c1a5b
abrandon <abrandon@ukho.gov.uk>
2020-07-09 11:41:36 +0100

    tf main update

--- terraform/main.tf
+++ terraform/main.tf
@@ -22,7 +22,7 @@ resource "azurerm_subnet" "aib-subnet" {
   name                                           = var.subnetName
   resource_group_name                            = azurerm_resource_group.aib-rg.name
   virtual_network_name                           = azurerm_virtual_network.aib-vnet.name
-  enforce_private_link_endpoint_network_policies = true
+  enforce_private_link_endpoint_network_policies = false
   address_prefix                                 = "10.1.2.0/25"
 }
 

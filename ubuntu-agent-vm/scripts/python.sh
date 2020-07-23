5ca07be24def1874b6f9a60ed54d91a39633620c
Marc Denman <MarcDenman@users.noreply.github.com>
2020-07-23 10:23:29 +0100

    Update python.sh

--- ubuntu-agent-vm/scripts/python.sh
+++ ubuntu-agent-vm/scripts/python.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env sh
+
+export DEBIAN_FRONTEND=noninteractive
 echo "install python"
 sudo apt-get install -y --no-install-recommends python python-dev python-pip python3 python3-dev python3-pip
+

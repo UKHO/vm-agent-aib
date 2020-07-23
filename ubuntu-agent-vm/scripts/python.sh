1fa094784ebddc1daa529edf8f195d5a7fdf65be
Marc Denman <MarcDenman@users.noreply.github.com>
2020-07-23 09:56:37 +0100

    Update python.sh

--- ubuntu-agent-vm/scripts/python.sh
+++ ubuntu-agent-vm/scripts/python.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 echo "install python"
-apt-get install -y --no-install-recommends python python-dev python-pip python3 python3-dev python3-pip
+sudo apt-get install -y --no-install-recommends python python-dev python-pip python3 python3-dev python3-pip

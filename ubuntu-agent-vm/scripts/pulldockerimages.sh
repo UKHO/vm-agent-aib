0c7ddb6401bdc78aa781a1ad9ea79e183290ef38
Marc Denman <MarcDenman@users.noreply.github.com>
2020-08-18 09:27:21 +0100

    Fix typo in image name (#29)

--- ubuntu-agent-vm/scripts/pulldockerimages.sh
+++ ubuntu-agent-vm/scripts/pulldockerimages.sh
@@ -4,4 +4,4 @@ echo "Install UKHO Snappy image"
 docker pull ukhydrographicoffice/esa-snap7-snappy
 
 echo "Install UKHO Terraform"
-docker pull ukhydrographicoffice/terrafrom
+docker pull ukhydrographicoffice/terraform

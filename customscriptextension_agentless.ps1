99b32a99e8ec49d0ccfad99289d80dd0751237b4
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2020-09-16 14:50:37 +0100

    Agentless customscripts

---
+++ customscriptextension_agentless.ps1
@@ -0,0 +1,48 @@
+param(
+    [Parameter(Mandatory)]
+    $owaspStorageAccount,
+    [Parameter(Mandatory)]
+    $owaspPassword,
+    $PartitionSize = 128
+
+)
+Write-Information "###### Remove old MMA certifcates ######"
+Get-Service "HealthService" | Stop-Service
+
+Move-Item "C:\Program Files\Microsoft Monitoring Agent\Agent\Health Service State" -Destination "C:\Program Files\Microsoft Monitoring Agent\Agent\Health Service State.old"
+Move-Item "C:\Program Files\Microsoft Monitoring Agent\Agent\AzureAutomation" -Destination "C:\Program Files\Microsoft Monitoring Agent\Agent\AzureAutomation.old"
+Get-Item -Path "HKLM:\SOFTWARE\Microsoft\HybridRunbookWorker" | Remove-Item -Recurse
+
+Set-Location 'Cert:\LocalMachine\Microsoft Monitoring Agent'
+
+Get-ChildItem | Remove-Item -Recurse
+
+Write-Information "###### Expand out the drive ######"
+$size = $PartionSize + "GB"
+Resize-Partition -DiskNumber 0 -PartitionNumer 2 -Size ($size)
+
+Write-Information "###### Get latest NVD ######"
+# Add nvd dc
+# Save the password so the drive will persist on reboot
+Invoke-Expression -Command "cmdkey /add:$owaspStorageAccount.file.core.windows.net /user:AZURE\$owaspStorageAccount /pass:$owaspPassword"
+
+# Mount the drive
+#net use Z: \\$owaspStorageAccount.file.core.windows.net\owaspdependencycheck $owaspPassword /user:Azure\$owaspStorageAccount
+New-PSDrive -Name Z -PSProvider FileSystem -Root "\\$owaspStorageAccount.file.core.windows.net\owaspdependencycheck" -Persist
+
+New-Item C:\dependency-check -ItemType Directory
+
+Copy-Item -Path Z:\dependency-check\* -Destination C:\dependency-check -Recurse
+
+
+Write-Information "###### ADD dc to path ######"
+# Save the password so the drive will persist on reboot
+$dcpath = "C:\dependency-check\bin"
+
+$path = [System.Environment]::GetEnvironmentVariable("Path", "Machine")
+[System.Environment]::SetEnvironmentVariable("Path", $path + ";$dcpath", "Machine")
+
+Write-Information "###### Check canary Capability ######"
+if ($env:ComputerName -like "win19-c*") {
+    [System.Environment]::SetEnvironmentVariable("CANARY", "Yes", "Machine")
+}
---
+++ customscriptextension_agentless.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+echo "########### CONFIGURING SWAP FILE ###########"
+sed -i 's/ResourceDisk.Format=n/ResourceDisk.Format=y/' /etc/waagent.conf
+sed -i 's/ResourceDisk.EnableSwap=n/ResourceDisk.EnableSwap=y/' /etc/waagent.conf
+sed -i 's/ResourceDisk.SwapSizeMB=0/ResourceDisk.SwapSizeMB=8192/' /etc/waagent.conf
+
+umount /mnt
+service walinuxagent restart
+
+sleep 20s
+
+echo '########### SETUP NVD DC ###########'
+mountpoint="owaspdependencycheck"
+storageaccountname=$4
+storageaccountfileshare="owaspdependencycheck"
+storageaccountuser=$5
+storageaccountpassword=$6
+
+echo "${storageaccountname}"
+
+echo "${storageaccountuser}"
+
+mkdir /mnt/${mountpoint}
+
+install -d /etc/smbcredentials
+
+fifo=$(mktemp)
+mkfifo ${fifo}
+install --mode=600 ${fifo} "/etc/smbcredentials/${storageaccountuser}.cred" &
+cat <<EOF > ${fifo}
+username=${storageaccountuser}
+password=${storageaccountpassword}
+EOF
+rm ${fifo}
+
+echo "//${storageaccountname}.file.core.windows.net/${storageaccountfileshare} /mnt/${mountpoint} cifs nofail,vers=3.0,credentials=/etc/smbcredentials/${storageaccountuser}.cred,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab
+mount -t cifs //${storageaccountname}.file.core.windows.net/${storageaccountfileshare} /mnt/${mountpoint} -o vers=3.0,credentials=/etc/smbcredentials/${storageaccountuser}.cred,dir_mode=0777,file_mode=0777,serverino
+
+export PATH=$PATH:/mnt/${mountpoint}/dependency-check/bin
+
+echo "Allow agent to run as root"
+export AGENT_ALLOW_RUNASROOT="YES"

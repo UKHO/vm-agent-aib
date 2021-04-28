bc2f75390067334d94896be7c6819b8fe064965f
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2021-04-28 10:48:29 +0100

    Attempt to get latest docker

--- ubuntu-agent-vm/scripts/docker.sh
+++ ubuntu-agent-vm/scripts/docker.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-echo "install docker"
-apt install -y docker.io
+apt-get update
+
+apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+
+apt-get update
+apt-get install docker-ce docker-ce-cli containerd.io

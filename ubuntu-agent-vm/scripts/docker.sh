0cc86aa91e9f95591550795e6726c27e0823796d
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2021-06-29 23:38:41 +0100

    Latest docker (#49)
    * Attempt to get latest docker
    
    * Use moby cli

--- ubuntu-agent-vm/scripts/docker.sh
+++ ubuntu-agent-vm/scripts/docker.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-echo "install docker"
-apt install -y docker.io
+apt-get remove -y moby-engine moby-cli
+apt-get update
+apt-get install -y moby-engine moby-cli
+apt-get install --no-install-recommends -y moby-buildx
+
+# Enable docker.service
+systemctl is-active --quiet docker.service || systemctl start docker.service
+systemctl is-enabled --quiet docker.service || systemctl enable docker.service
+
+# Docker daemon takes time to come up after installing
+sleep 10
+docker info

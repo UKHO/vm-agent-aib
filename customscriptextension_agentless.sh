6458794a14dbc8a76330ae57b007cd00a31d1667
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2021-07-06 10:52:48 +0100

    More sleeps

--- customscriptextension_agentless.sh
+++ customscriptextension_agentless.sh
@@ -34,6 +34,8 @@ password=${storageaccountpassword}
 EOF
 rm ${fifo}
 
+sleep 10s
+
 echo "//${storageaccountname}.file.core.windows.net/${storageaccountfileshare} /mnt/${mountpoint} cifs nofail,vers=3.0,credentials=/etc/smbcredentials/${storageaccountuser}.cred,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab
 mount -t cifs //${storageaccountname}.file.core.windows.net/${storageaccountfileshare} /mnt/${mountpoint} -o vers=3.0,credentials=/etc/smbcredentials/${storageaccountuser}.cred,dir_mode=0777,file_mode=0777,serverino
 

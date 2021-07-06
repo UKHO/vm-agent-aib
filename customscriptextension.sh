f9262be17e9cd6574b4046928b9dcd24b32dc6f2
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2021-07-06 10:53:24 +0100

    More sleeps

--- customscriptextension.sh
+++ customscriptextension.sh
@@ -34,14 +34,10 @@ password=${storageaccountpassword}
 EOF
 rm ${fifo}
 
+sleep 10s
+
 echo "//${storageaccountname}.file.core.windows.net/${storageaccountfileshare} /mnt/${mountpoint} cifs nofail,vers=3.0,credentials=/etc/smbcredentials/${storageaccountuser}.cred,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab
 mount -t cifs //${storageaccountname}.file.core.windows.net/${storageaccountfileshare} /mnt/${mountpoint} -o vers=3.0,credentials=/etc/smbcredentials/${storageaccountuser}.cred,dir_mode=0777,file_mode=0777,serverino
-
-if [ ! -d "/mnt/${mountpoint}/dependency-check" ] {
-  sleep 1m
-  echo "//${storageaccountname}.file.core.windows.net/${storageaccountfileshare} /mnt/${mountpoint} cifs nofail,vers=3.0,credentials=/etc/smbcredentials/${storageaccountuser}.cred,dir_mode=0777,file_mode=0777,serverino" >> /etc/fstab
-  mount -t cifs //${storageaccountname}.file.core.windows.net/${storageaccountfileshare} /mnt/${mountpoint} -o vers=3.0,credentials=/etc/smbcredentials/${storageaccountuser}.cred,dir_mode=0777,file_mode=0777,serverino
-}
 
 sleep 10s
 

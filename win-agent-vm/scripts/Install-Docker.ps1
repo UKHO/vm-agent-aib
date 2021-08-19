3ddf8b4341f5c91a8f58869a76d061f3f0faab85
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2021-08-19 16:11:54 +0100

    Create Install-Docker.ps1

---
+++ win-agent-vm/scripts/Install-Docker.ps1
@@ -0,0 +1,20 @@
+################################################################################
+##  File:  Install-Docker.ps1
+##  Desc:  Install Docker.
+##         Must be an independent step becuase it requires a restart before we
+##         can continue.
+################################################################################
+
+Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
+Write-Host "Install-Module DockerProvider"
+Install-Module DockerMsftProvider -Force
+
+Write-Host "Install-Package Docker"
+Install-Package -Name docker -ProviderName DockerMsftProvider -Force
+Start-Service docker
+
+Write-Host "Install-Package Docker-Compose"
+Choco-Install -PackageName docker-compose
+
+Write-Host "Install Helm"
+Choco-Install -PackageName kubernetes-helm

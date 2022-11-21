024c221b8d15fb2cfc8afe2ed6bacb0a04201ec7
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2022-11-21 15:28:44 +0000

    Update baseupdates.sh

--- ubuntu-2204-agent-vm/scripts/baseupdates.sh
+++ ubuntu-2204-agent-vm/scripts/baseupdates.sh
@@ -4,4 +4,4 @@ echo "update apt"
 apt update
 
 echo "install baseline"
-apt install -y unzip dotnet6 dotnet-sdk-6.0 liblttng-ust0 libkrb5-3 zlib1g debsums
+apt install -y unzip dotnet6 dotnet-sdk-6.0

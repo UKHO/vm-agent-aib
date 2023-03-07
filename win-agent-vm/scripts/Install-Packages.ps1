1fcdb18dfd41dd0c7e98f67e89cd5743e21be30b
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2023-03-07 15:53:38 +0000

    Add ndp

--- win-agent-vm/scripts/Install-Packages.ps1
+++ win-agent-vm/scripts/Install-Packages.ps1
@@ -1,1 +1,8 @@
 
+################################################################################
+##  File:  Install-Oackages.ps1
+##  Desc:  Install Chocolatey Packages.
+################################################################################
+
+Write-Host "Install ndp 4.8"
+choco install netfx-4.8-devpack

fa9199126daa2aff591790a764782e735344f178
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2023-03-24 15:36:07 +0000

    Adding yarn

---
+++ ubuntu-agent-vm/scripts/yarn.sh
@@ -0,0 +1,16 @@
+apt update
+
+apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+apt update
+
+apt install yarn

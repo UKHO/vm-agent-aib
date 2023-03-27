0f5527463120910c30a715d4209ebba280d96975
Luke Duddridge <luke.duddridge@ukho.gov.uk>
2023-03-27 17:06:28 +0100

    Set nodejs to v10

--- ubuntu-agent-vm/scripts/nodejs.sh
+++ ubuntu-agent-vm/scripts/nodejs.sh
@@ -1,3 +1,14 @@
 apt update
-apt install -y nodejs
-apt install -y npm
+
+apt install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+source ~/.bashrc
+
+nvm install lts/dubnium

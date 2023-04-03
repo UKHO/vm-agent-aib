8020c529cd9f1b0e25e929160d1b8f3434f5ac5d
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2023-04-03 09:26:05 +0100

    Update Agent Downloads

--- ubuntu-agent-vm/scripts/azuredevopsagent.sh
+++ ubuntu-agent-vm/scripts/azuredevopsagent.sh
@@ -5,7 +5,7 @@ mkdir -p $agtLocation
 mkdir -p $agtLocation/_work
 
 echo "download agent"
-curl https://vstsagentpackage.azureedge.net/agent/2.200.2/vsts-agent-linux-x64-2.200.2.tar.gz | tar zx -C $agtLocation
+curl https://vstsagentpackage.azureedge.net/agent/3.218.2/vsts-agent-linux-x64-3.218.2.tar.gz | tar zx -C $agtLocation
 
 echo "set permissions on agent directory"
 chmod 755 -R $agtLocation

9dec36d60c86c23a92412f02ab5721acefea7085
Steve.Aston <saston2003@outlook.com>
2023-07-12 14:02:20 +0100

    Removed redudant files from 2022 building

--- win-2022-vs-agent-vm-ARCHIVED/scripts/Install-Packages.ps1
+++
@@ -1,7 +0,0 @@
-
-################################################################################
-##  File:  Install-Oackages.ps1
-##  Desc:  Install Chocolatey Packages.
-################################################################################
-
-Write-Host "Installing Using Chocolatey."

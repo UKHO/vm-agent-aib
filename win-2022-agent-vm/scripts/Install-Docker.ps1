bc107c6d2a9c066c01e5be5754767b6e53e622b1
Steve.Aston <saston2003@outlook.com>
2023-08-09 11:48:52 +0100

    Install Docker and Dockerd

--- win-2022-agent-vm/scripts/Install-Docker.ps1
+++ win-2022-agent-vm/scripts/Install-Docker.ps1
@@ -4,16 +4,19 @@
 ##         Must be an independent step becuase it requires a restart before we
 ##         can continue.
 ################################################################################
-#
-#https://lippertmarkus.com/2021/09/04/containers-without-docker-desktop/
 
-Enable-WindowsOptionalFeature -Online -FeatureName containers –All
-Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Hyper-V –All
+Set-PSRepository -InstallationPolicy Trusted -Name PSGallery
+#Write-Host "Install Docker"
+#choco install docker-for-windows
 
-curl.exe -o docker.zip -LO https://download.docker.com/win/static/stable/x86_64/docker-20.10.13.zip 
-Expand-Archive docker.zip -DestinationPath C:\
-[Environment]::SetEnvironmentVariable("Path", "$($env:path);C:\docker", [System.EnvironmentVariableTarget]::Machine)
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine")
-dockerd --register-service
-Start-Service docker
-docker run hello-world
+Write-Host "Install-Package Docker-Compose"
+choco install docker-compose
+
+Write-Host "Install Docker CLI"
+choco install docker-cli
+
+Write-Host "Install Docker Engine"
+choco install docker-engine
+
+Write-Host "Install Helm"
+choco install kubernetes-helm

08df556d13e19603f7b75f6e6e523c86b1b26c30
Steve.Aston <saston2003@outlook.com>
2023-08-09 11:46:36 +0100

    Install docker and dockerd

---
+++ win-2022-agent-vm/scripts/Install-dockerd.ps1
@@ -0,0 +1,3 @@
+#this does a reboot
+Invoke-WebRequest -UseBasicParsing "https://raw.githubusercontent.com/microsoft/Windows-Containers/Main/helpful_tools/Install-ContainerdRuntime/install-containerd-runtime.ps1" -o install-containerd-runtime.ps1
+.\install-containerd-runtime.ps1

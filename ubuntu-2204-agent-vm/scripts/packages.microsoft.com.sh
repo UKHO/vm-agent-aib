2456ab93b390345ba71baf897a4468c8b6ecd30a
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2023-11-22 17:17:12 +0000

    remove &&

--- ubuntu-2204-agent-vm/scripts/packages.microsoft.com.sh
+++ ubuntu-2204-agent-vm/scripts/packages.microsoft.com.sh
@@ -2,14 +2,13 @@ curl -sL https://packages.microsoft.com/keys/microsoft.asc | \
     gpg --dearmor | \
     tee /etc/apt/trusted.gpg.d/microsoft.asc.gpg > /dev/null
 
-echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | \
-    tee /etc/apt/sources.list.d/azure-cli.list
+echo "deb [arch=amd64] https://packages.microsoft.com/repos/azure-cli/ $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/azure-cli.list
 
-apt-get update \
-    && apt-get install -y --no-install-recommends azure-cli=* \
-    && rm -rf /var/lib/apt/lists/*
+apt-get update
+apt-get install -y --no-install-recommends azure-cli=*
+rm -rf /var/lib/apt/lists/*
 
-sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-buster-prod/ buster main" > /etc/apt/sources.list.d/microsoft.list' \
-    && apt-get update \
-    && apt-get install -y --no-install-recommends powershell=* \    
-    && rm -rf /var/lib/apt/lists/*
+sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/microsoft-debian-buster-prod/ buster main" > /etc/apt/sources.list.d/microsoft.list'
+apt-get update
+apt-get install -y --no-install-recommends powershell=*
+rm -rf /var/lib/apt/lists/*

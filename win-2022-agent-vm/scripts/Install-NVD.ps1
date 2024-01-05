07e94336e6cdc1435f29c1a87aaa4657a1876d5c
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2024-01-05 15:26:45 +0000

    logging text

--- win-2022-agent-vm/scripts/Install-NVD.ps1
+++ win-2022-agent-vm/scripts/Install-NVD.ps1
@@ -1,5 +1,5 @@
 param($NvdApiKey = "<NvdApiKey>")
-"SET TEMP"
+Write-Host "Install NVD Checker"
 Set-Location "C:\Temp"
 [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.SecurityProtocolType]::Tls12;
 $latest = Invoke-RestMethod "https://api.github.com/repos/jeremylong/DependencyCheck/releases/latest"
@@ -10,7 +10,7 @@ $release = $latest | `
 if ($null -ne $release) {
     $url = $release.browser_download_url
 
-    "Release $url"
+    Write-Host "Release $url"
     $leaf = Split-Path $url -Leaf
 
     Invoke-Command -ScriptBlock { 
@@ -24,23 +24,24 @@ if ($null -ne $release) {
     # downloaded and saved from releases: https://github.com/jeremylong/DependencyCheck/releases
     #Copy-Item "\\mgmt.local\dfs\DML-SW-Engineering\OWASP Dependency Check\$zip" $WorkingDirectory -Force
 
-    "Expand zip"
+    Write-Host "Expand zip"
     Expand-Archive .\$leaf -Force
 
-    "Move Temp to root"
+    Write-Host "Move Temp to root"
     Move-Item .\$dir\dependency-check .\dependency-check -Force
 
     $content = Get-Content '.\dependency-check\bin\dependency-check.bat'
 
     $content = $content.Replace("org.owasp.dependencycheck.App %CMD_LINE_ARGS%", "org.owasp.dependencycheck.App %CMD_LINE_ARGS% -n")
 
-    "Set content with no update"
+    Write-Host "Set content with no update"
     Set-Content -Value $content -Path .\dependency-check\bin\dependency-check.bat
 
     .\dependency-check\bin\dependency-check.bat --update --nvdApiKey $NvdApiKey
     $currentPath = $Env:Path
     $owasppath = "C:\dependency-check\bin\"
 
+    Write-Host "Adding $owasppath to environment path"
     $currentPath += $owasppath
 
     [Environment]::SetEnvironmentVariable("Path", $currentPath, "Machine") 

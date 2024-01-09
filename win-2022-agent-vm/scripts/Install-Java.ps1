c10d6de596ec418a19ed598a1e36f60326867188
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2024-01-09 15:59:35 +0000

    Create Install-Java.ps1

---
+++ win-2022-agent-vm/scripts/Install-Java.ps1
@@ -0,0 +1,1 @@
+choco install adoptopenjdk --version 13.101.9 --params="/ADDLOCAL=FeatureMain,FeatureEnvironment,FeatureJarFileRunWith,FeatureJavaHome INSTALLDIR=c:\Program Files\AdoptOpenJDK\ /quiet"

4c396bc0cd99e331d3ed14b495c8c90a57a669a0
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2024-10-24 16:38:00 +0100

    Create dotnet.sh

---
+++ ubuntu-2204-agent-vm/scripts/dotnet.sh
@@ -0,0 +1,15 @@
+echo "Update package list"
+apt update
+
+echo "Install prerequisites"
+apt install -y wget apt-transport-https
+
+echo "Add Microsoft repository"
+wget https://packages.microsoft.com/config/ubuntu/22.04/packages-microsoft-prod.deb -O packages-microsoft-prod.deb
+dpkg -i packages-microsoft-prod.deb
+
+echo "Update package list after adding repository"
+apt update
+
+echo "Install baseline packages"
+apt install -y dotnet-sdk-8.0

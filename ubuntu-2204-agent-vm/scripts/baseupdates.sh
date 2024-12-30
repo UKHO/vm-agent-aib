73bae08b1fd54e923b4b435032b845aaa53e1dfb
Luke Duddridge <338863+elduddz@users.noreply.github.com>
2024-12-30 11:50:01 +0000

    checking apt is updated

--- ubuntu-2204-agent-vm/scripts/baseupdates.sh
+++ ubuntu-2204-agent-vm/scripts/baseupdates.sh
@@ -3,5 +3,13 @@
 echo "update apt"
 apt update
 
+echo "check"
+if ! apt check; then
+    echo "Issues detected. Running 'apt update' again"
+    apt update
+else
+    echo "System is healthy."
+fi
+
 echo "install unzip"
 apt install -y unzip
